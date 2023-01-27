400187b24b65385aac7c6f2c7e420db83568ef29
Lucas X <lucasmxisto@outlook.com>
2023-01-27 17:36:16 -0300

    Requisito 3 - Concluído

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      cardName: '',
-      cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
-      cardImage: '',
-      cardRare: '',
-      cardTrunfo: false,
+      cardName: 'Alow',
+      cardDescription: 'Descrição',
+      cardAttr1: '1',
+      cardAttr2: '2',
+      cardAttr3: '3',
+      cardImage: 'https://styles.redditmedia.com/t5_2su6s/styles/communityIcon_4g1uo0kd87c61.png?width=256&s=3f7493995143d3cf40b1fedc582607cea194b579',
+      cardRare: 'normal',
+      cardTrunfo: true,
       hasTrunfo: false,
       isSaveButtonDisabled: false,
       onInputChange: (() => {}),
@@ -50,6 +51,16 @@ class App extends React.Component {
           isSaveButtonDisabled={ isSaveButtonDisabled }
           onInputChange={ onInputChange }
           onSaveButtonClick={ onSaveButtonClick }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
         />
       </div>
     );
